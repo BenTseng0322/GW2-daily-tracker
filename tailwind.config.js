1f775fd6aca288c056f185a934877f634f6bcff2
BenTseng0322 <banm0322@hotmail.com>
2023-06-12 15:25:58 +0800

    feature: 安裝 flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./build/*.js"],
+  content: [
+    "./*.html",
+    "./build/*.js",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
